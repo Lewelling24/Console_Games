664d11885d7b4c680f70761e5eec7febcd14198a
Kane Lewelling <kanelewelling@gmail.com>
2024-04-10 09:48:08 -0400

    Add files via upload

--- Guess_The_Num.py
+++ Guess_The_Num.py
@@ -10,25 +10,43 @@ class Guess_The_Num:
 
         computer_choice = random.randrange(0,101)
 
-        player_choice = int(input("Please choose a number between 0 and 100 "))
+        guess_count = 0
 
-        if(player_choice == computer_choice):
-            print("You guessed the correct number, you win!")
+        while(guess_count < 3):
+
+            player_choice = int(input("Please choose a number between 0 and 100 "))
+
+            if(player_choice == computer_choice):
+                print("You guessed the correct number, you win!")
+                break
+            else:
+                print("You did not guess the correct number!")
+                guess_count += 1
+        if(guess_count < 3):
             return 'W'
         else:
-            print("You did not guess the correct number, you lose!")
+            print("You did not guess the correct number in three tries, you lose!")
             return 'L'
+                
         
     #This function will have the computer try and guess your number
     def Guess_The_Number_Computer():
         player_choice = int(input("Please choose a number between 0 and 100 "))
 
-        computer_choice = random.randrange(0,101)
+        guess_count = 0
 
-        if(computer_choice == player_choice):
-            print("The computer was able to guess your number, you lose!")
+        while (guess_count < 3):
+            computer_choice = random.randrange(0,101)
+
+            if(computer_choice == player_choice):
+                print("The computer was able to guess your number, you lose!")
+                break
+            else:
+                print("The computer did not guess your number")
+                guess_count += 1
+        if(guess_count < 3):
             return 'L'
         else:
-            print("The computer did not guess your number, you win!")
+            print("The computer was unable to guess your number in three tries, you win!")
             return 'W'
 
