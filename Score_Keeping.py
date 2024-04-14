89cc7e5d47d56f3de9e6925b84a5549eb5cbd21d
Kane Lewelling <kanelewelling@gmail.com>
2024-04-14 17:24:28 -0400

    Update Score_Keeping.py
    Added beginnings of functionality for score keeping

--- Score_Keeping.py
+++ Score_Keeping.py
@@ -1,1 +1,37 @@
 #todo implement score keeping class for use with game classes
+
+class Score_Keeper():
+  
+  #player_score, computer_score  = 0
+
+  def __init__(self):
+    self.player_score = 0
+    self.computer_score = 0
+
+  # constructor initializing player, and computer scores to a chosen number for counting down
+  def __init__(self, starting_score):
+    self.player_score = starting_score
+    self.computer_score = starting_score
+  
+  # increment palyer, and computer score tallies
+  def keep_count(character):
+    if (character == 'W'):
+      player_score += 1
+    else:
+      computer_score += 1
+
+  # countdown scores
+  def countdown_score(character)
+    if(character == 'W'):
+      computer_score -= 1
+    else:
+      player_score -= 1
+  
+  # based on computer, and player scores return C or P for winner, and T for tie
+  def winner_determination():
+    if( player_choice == computer_choice):
+      return 'T'
+    elif( player_score > computer_score):
+      return 'P'
+    else:
+      return 'C'
