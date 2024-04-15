ab7dfe41d60d0393ec3de6692c2e5b99972033c1
Kane Lewelling <kanelewelling@gmail.com>
2024-04-15 15:10:05 -0400

    Add files via upload
    edited some of the functions

--- Score_Keeping.py
+++ Score_Keeping.py
@@ -2,11 +2,21 @@
 
 class Score_Keeper():
   
-  #player_score, computer_score  = 0
+  player_score = 0 
+  computer_score = 0
 
   def __init__(self):
     self.player_score = 0
     self.computer_score = 0
+
+    # based on computer, and player scores return C or P for winner, and T for tie
+  def return_winner(self):
+    if(self.player_score == self.computer_score):
+      return 'T'
+    elif (self.player_score > self.computer_score):
+      return 'P'
+    else:
+      return 'C'
 
   # constructor initializing player, and computer scores to a chosen number for counting down
   def __init__(self, starting_score):
@@ -14,26 +24,20 @@ class Score_Keeper():
     self.computer_score = starting_score
   
   # increment palyer, and computer score tallies
-  def keep_count(character):
+  def keep_count(self,character):
     if (character == 'W'):
-      player_score += 1
-    elif(character -- 'C':
-      computer_score += 1
+      self.player_score += 1
+    elif(character == 'C'):
+      self.computer_score += 1
     else:
       print('Neither player wins, scores not updated')
 
   # countdown scores
-  def countdown_score(character)
+  def countdown_score(self,character):
     if(character == 'W'):
-      computer_score -= 1
+      self.computer_score -= 1
     else:
-      player_score -= 1
+      self.player_score -= 1
   
-  # based on computer, and player scores return C or P for winner, and T for tie
-  def winner_determination():
-    if( player_choice == computer_choice):
-      return 'T'
-    elif( player_score > computer_score):
-      return 'P'
-    else:
-      return 'C'
+  
+
