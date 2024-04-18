76046964032c34460c2d46338e2e08b6fdebc024
Kane Lewelling <kanelewelling@gmail.com>
2024-04-17 20:35:53 -0400

    Add files via upload
    Added time library to slow down the game, added some extra text to add context during play

--- Console_Games/Rock_Paper_Scissors.py
+++ Console_Games/Rock_Paper_Scissors.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 class rock_paper_scissors:
     #prints win/lose/tie to the console, returns character for outcome
     #returns P for player win, C for computer win, T for tie
@@ -9,9 +9,21 @@ class rock_paper_scissors:
     
     def rock_paper_scissors():
         computer_player_choice = random.randrange(0, 3)
-
+        
         player_choice = input("Please choose Rock, Paper or Scissors: ")
 
+        time.sleep(1)
+        
+        if(computer_player_choice == 0):
+            print("The Computer chose Rock.")
+        elif(computer_player_choice == 1):
+            print("The Computer chose Paper.")
+        else:
+            print("The Computer chose Scissors.")
+
+        time.sleep(1)
+
+        print("You chose " + player_choice + ".")
         match player_choice:
             case "Rock":
                 player_choice = 0
@@ -19,6 +31,9 @@ class rock_paper_scissors:
                 player_choice = 1
             case "Scissors":
                 player_choice = 2
+
+        
+        time.sleep(1)
 
         if(player_choice == computer_player_choice): #Tie first to save time
             print("It's a tie!")
